a4550f828312279c5432b7fc47e26390ce7feb7c
Alexander Zalutskiy <metalhead.sanya@gmail.com>
2015-12-21 01:19:17 +0600

    Add basic of GtkGrid

---
+++ src/Grid.swift
@@ -0,0 +1,23 @@
+//
+// Created by Alexander Zalutskiy on 21.12.15.
+// Copyright (c) 2015 Metalhead. All rights reserved.
+//
+
+import CGTK
+
+class Grid: Container {
+	internal var n_Grid: UnsafeMutablePointer<GtkGrid>
+
+	internal init(n_Grid: UnsafeMutablePointer<GtkGrid>) {
+		self.n_Grid = n_Grid
+		super.init(n_Container: unsafeBitCast(self.n_Grid, UnsafeMutablePointer<GtkContainer>))
+	}
+
+	convenience init() {
+		self.init(n_Grid: unsafeBitCast(gtk_grid_new()), UnsafeMutablePointer<GtkGrid>)
+	}
+
+	func attachWidget(widget: Widget, left: Int, top: Int, width: Int, height: Int) {
+		gtk_grid_attach(n_Grid, widget.n_Widget, Int32(left), Int32(top), Int32(width), Int32(height))
+	}
+}
