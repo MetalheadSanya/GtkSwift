7f91f3926615177ab6fb5def97389a2f41ce6a20
Alexander Zalutskiy <metalhead.sanya@gmail.com>
2015-12-21 01:28:53 +0600

    Test proj

--- src/main.swift
+++ src/main.swift
@@ -1,25 +1,31 @@
 import CGTK
 
+let buttonCallback = { (button: Button, userData: gpointer) in
+	print("Hellow, World!")
+}
+
 let app = Application(applicationId: "org.gtk.example", flags: [])!
 app.activateCallbacks.append({ (app: Application, userData: gpointer) -> Void in
 	let window = ApplicationWindow(application: app)
-	window.title = "Test"
-	// window.defaultSize = Size(width: 200, height: 200)
+	window.title = "Window"
+	window.borderWidth = 10
 
-	let buttonBox = ButtonBox(orientation: .Horizontal)
-	window.add(buttonBox)
+	let grid = Grid()
+	window.add(grid)
 
-	let button = Button(label: "Hello, World!")
+	let button = Button(label: "Button 1")
+	button.clickedCallbacks.append(buttonCallback)
+	grid.attachWidget(button, left: 0, top: 0, width: 1, height: 1)
 
-	button.clickedCallbacks.append({ (button: Button, userData: gpointer) in 
-		print("Hello, World!")
-	})
+	let button2 = Button(label: "Button 2")
+	button2.clickedCallbacks.append(buttonCallback)
+	grid.attachWidget(button2, left: 1, top: 0, width: 1, height: 1)
 
-	button.clickedCallbacks.append({ (button: Button, userData: gpointer) in 
+	let quitButton = Button(label: "Quit")
+	quitButton.clickedCallbacks.append({ (button: Button, userData: gpointer) in
 		window.destroy()
 	})
-
-	buttonBox.add(button)
+	grid.attachWidget(quitButton, left: 0, top: 1, width: 2, height: 1)
 
 	window.showAll()
 })
