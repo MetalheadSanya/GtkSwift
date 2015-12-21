4111cd416f34d3c410eefdd70228c2ea2045e560
Alexander Zalutskiy <metalhead.sanya@gmail.com>
2015-12-21 11:14:31 +0600

    fix wrapped buttons

--- src/Dialog.swift
+++ src/Dialog.swift
@@ -22,8 +22,10 @@ class Dialog: Window {
 		self.init(n_Dialog: unsafeBitCast(gtk_dialog_new_with_buttons(title, parent.n_Window, UInt32(flags), nil),
 				UnsafeMutablePointer<GtkDialog>.self))
 
-		for button in buttons? {
-			addButtonWithText(button.0, response: button.1)
+		if let buttons = buttons {
+			for button in buttons {
+				addButtonWithText(button.0, response: button.1)
+			}
 		}
 	}
 
