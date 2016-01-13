8d1f69174785526f9eecc3b653d37c08c93483a8
Alexander Zalutskiy <metalhead.sanya@gmail.com>
2016-01-14 01:10:59 +0600

    overriding test

--- src/Widget.swift
+++ src/Widget.swift
@@ -47,7 +47,7 @@ class Widget {
 		overrideGtkHandler()
 	}
 
-	private var gtk_widget_destroy_real: CDestroyFunc
+	private var gtk_widget_destroy_real: CDestroyFunc!
 
 	private func getGtkWidgetClass() -> UnsafeMutablePointer<GtkWidgetClass> {
 		return unsafeBitCast(unsafeBitCast(n_Widget, UnsafeMutablePointer<GTypeInstance>.self).memory.g_class,
