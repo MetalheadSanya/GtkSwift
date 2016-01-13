03a34a49b35b82ecc7ef657eda6cb654d919bedc
Alexander Zalutskiy <metalhead.sanya@gmail.com>
2016-01-13 18:02:48 +0600

    fix getting type func for GtkBin

--- src/GTypeHelper.swift
+++ src/GTypeHelper.swift
@@ -3,10 +3,10 @@ import CGTK
 internal class GTypeHelper {
 	private static let widgetType: GType = gtk_widget_get_type()
 	private static let containerType = gtk_container_get_type()
-	private static let binType = gtk_bin_get_child_type()
+	private static let binType = gtk_bin_get_type()
 
 	class func convertToClass(type: GType) -> AnyObject.Type? {
-		
+
 
 		if type == widgetType {
 			return Widget.self
