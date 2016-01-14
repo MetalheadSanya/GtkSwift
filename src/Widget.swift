fbd687a7261a9c406f83ad2c38f1b2140b815931
Alexander Zalutskiy <metalhead.sanya@gmail.com>
2016-01-14 23:25:08 +0600

    Override test

--- src/Widget.swift
+++ src/Widget.swift
@@ -71,7 +71,7 @@ class Widget {
 
 	func destroy() {
 		print("destroy widget")
-		WidgetNotificationCenter.sharedInstance.registerTypes[String(self.dynamicType)].destroy(n_Widget)
+		WidgetNotificationCenter.sharedInstance.registerTypes[String(self.dynamicType)]!.destroy(n_Widget)
 	}
 
 	// TODO: some for gtk_widget_in_destruction(), gtk_widget_destroyed()
